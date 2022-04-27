24c75019ab5e71167334b06c6c28fc71f640aaf4
sbbbotgh <76098746+sbbbotgh@users.noreply.github.com>
2022-04-27 18:39:43 +0530

    Add files via upload

---
+++ pg2.js
@@ -0,0 +1,19 @@
+img = "";
+
+function preload(){
+    img = loadImage("park.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(500, 300);
+    canvas.center();
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+}
+
+
+function back(){
+    window.location = "/index.html";
+}
---
+++ pg5.js
@@ -0,0 +1,18 @@
+img = "";
+
+function preload(){
+    img = loadImage("shop.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(500, 300);
+    canvas.center();
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+}
+
+function back(){
+    window.location = "/index.html";
+}
